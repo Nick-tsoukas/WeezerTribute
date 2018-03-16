ee828f7f66edf3c1c60d168bbe3052e9951b7f9b
Nikolaos Tsoukas <nikolaostsoukas@Nikolaoss-iMac.local>
2018-03-15 23:20:44 -0600

    First commit

---
+++ main.js
@@ -0,0 +1,24 @@
+var menuItems = document.getElementsByClassName('menu_item');
+
+var home = document.getElementById('homeLine');
+var bio = document.getElementById('bioLine');
+
+document.addEventListener("DOMContentLoaded", function() {
+  bio.style.opacity = "0"
+});
+
+Array.from(menuItems).forEach(function(current,index, array){
+  current.addEventListener('click', function(e){
+    if(this.id === "homeHeader"){
+      home.style.opacity = "1";
+      bio.style.opacity = "0";
+    } else {
+      bio.style.opacity = "1";
+      home.style.opacity = "0";
+    }
+  })
+})
+// bioHeader.addEventListener("click", function(){
+//   bio.style.opacity = "block";
+//   console.log(bio)
+// })
