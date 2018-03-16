9ebbae11088e1abdac01820f35e81988b178eba6
Nikolaos Tsoukas <nikolaostsoukas@Nikolaoss-iMac.local>
2018-03-16 16:32:37 -0600

    Adding final layout and effects

--- main.js
+++ main.js
@@ -2,9 +2,11 @@ var menuItems = document.getElementsByClassName('menu_item');
 
 var home = document.getElementById('homeLine');
 var bio = document.getElementById('bioLine');
+var card = document.getElementById('myCard');
 
 document.addEventListener("DOMContentLoaded", function() {
-  bio.style.opacity = "0"
+  bio.style.opacity = "0";
+  card.style.opacity = "0";
 });
 
 Array.from(menuItems).forEach(function(current,index, array){
@@ -12,8 +14,10 @@ Array.from(menuItems).forEach(function(current,index, array){
     if(this.id === "homeHeader"){
       home.style.opacity = "1";
       bio.style.opacity = "0";
+      card.style.opacity = "0"
     } else {
       bio.style.opacity = "1";
+      card.style.opacity = "1";
       home.style.opacity = "0";
     }
   })
